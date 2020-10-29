6448e1acaff5aa2610f8cddd299ecb7f2beb1526
edward-pis-1670 <71159568+edward-pis-1670@users.noreply.github.com>
2020-10-29 16:57:56 +0700

    Codechoi

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import *
 from django.http import JsonResponse
 import json
+from datetime import datetime
 
 
 # Create your views here.
@@ -67,4 +68,29 @@ def updateItem(request):
     if orderItem.quantity <= 0:
         orderItem.delete()
     return JsonResponse('Item was added', safe=False)
+
+def processOrder(request):
+    transaction_id =datetime.now().timestamp()
+    data = json.loads(request.body)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        total = float(data['form']['total'])
+        order.transaction_id = transaction_id
+        if total == order.get_cart_total:
+            order.complete = True
+        order.save()
+
+        if order.shipping == True:
+            ShippingAdress.objects.create(
+                customer = customer,
+                order = order,
+                adress = data['shipping']['adress'],
+                city = data['shipping']['city'],
+                state = data['shipping']['state'],
+                zipcode = data['shipping']['zipcode'],
+            )
+    else:
+        print('User not logged in.')
+    return JsonResponse('Payment complete !', safe=False)
 
