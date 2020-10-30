9b4bfb06997733f23d68b1950e25786408a9951f
edward-pis-1670 <71159568+edward-pis-1670@users.noreply.github.com>
2020-10-30 17:00:43 +0700

    codechoi1

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -3,7 +3,8 @@ from .models import *
 from django.http import JsonResponse
 import json
 from datetime import datetime
-
+from .models import *
+from . utils import cookieCart
 
 # Create your views here.
 def store(request):
@@ -28,10 +29,40 @@ def cart(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
+        try:
+            cart = json.loads(request.COOKIES['cart'])
+        except:
+            cart= {}
+        print('Cart:',cart)
         items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        order = {'get_cart_total': 0, 'get_cart_items': 0,'shipping': False}
         cartItems = order['get_cart_items']
-    context = {'items': items, 'order': order, 'cartItems': cartItems,'shipping': False}
+        for i in cart:
+            try:
+                cartItems += cart[i]['quantity']
+                product = Product.objects.get(id=i)
+                total = (product.price * cart[i]['quantity'])
+                order['get_cart_total'] += total
+                order['get_cart_items'] += cart[i]['quantity']
+                item ={
+                    'product':{
+                        'id':product.id,
+                        'name':product.name,
+                        'price':product.price,
+                        'ImageURL':product.ImageURL,
+                        },
+                    'quantity':cart[i]['quantity'],
+                    'get_total': total
+                }
+                items.append(item)
+
+                if product.digital == False:
+                    order['shipping'] = True
+            except:
+                pass
+
+
+    context = {'items': items, 'order': order, 'cartItems': cartItems}
 
     return render(request, 'store/cart.html', context)
 
